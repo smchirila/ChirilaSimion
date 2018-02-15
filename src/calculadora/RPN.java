976ffbf22d4b69993719fd4bbd3873b65937326e
Chirila <Chirila@Chirila>
2018-02-15 17:57:13 +0100

    EDD Practiva

--- src/calculadora/RPN.java
+++ src/calculadora/RPN.java
@@ -1,18 +1,9 @@
 package calculadora;
 
-/** 
-Objetivo: Obtener código refactorizado a partir de otro que no lo está.
-Tarea: Para esta tarea, se refactorizará un programa mal escrito, sin
-cambiar la forma en que funciona el programa. Este programa, RPN.java
-es una calculadora de notación inversa polaca que utiliza una pila.
-
- Reverse Polish notation (RPN) Notación Polaca inversa, por ejemplo
-la expresión: 4 * 5 - 7 / 2 % 3 nos da 1,5 respetando la prioridad de
-los operadores en notación RPN seria: 4 5 * 7 2 / - 3 % (pues no podemos
-poner los paréntesis para alterar la prioridad)
-
- Se debe reorganizar este código usando al menos tres de las reglas
-vistas en clase.
+/**
+ * 
+ * @author Chirila
+ *
  */
 
 public class RPN {
